6222eb3c6c93a3ac1cff765ed9cce072a09556a9
alsabar92 <alsabar1992@gmail.com>
2019-11-21 10:48:10 +0100

    Final

--- wp-master.sh
+++ wp-master.sh
@@ -12,9 +12,9 @@ rm -r wordpress
 mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 echo "define ( 'WP_SITEURL', 'http://$IP_BALANCER' );" >> /var/www/html/wp-config.php
 echo "define ( 'WP_HOME', 'http://$IP_BALANCER' );" >> /var/www/html/wp-config.php
-sed -i 's/database_name_here/wordpress/' /var/www/html/wp-config.php
-sed -i 's/username_here/wpuser/' /var/www/html/wp-config.php
-sed -i 's/password_here/wpuser/' /var/www/html/wp-config.php
+sed -i 's/nombredetubasededatos/wordpress/' /var/www/html/wp-config.php
+sed -i 's/nombredeusuario/wpuser/' /var/www/html/wp-config.php
+sed -i 's/contraseña/wpuser/' /var/www/html/wp-config.php
 sed -i 's/localhost/3.208.18.253/' /var/www/html/wp-config.php
 
 
@@ -32,15 +32,9 @@ KEYS=$(curl https://api.wordpress.org/secret-key/1.1/salt/)
 KEYS=$(echo $KEYS | tr / _)
 sed -i "/#@-/a $KEYS" /var/www/html/wp-config.php
 
-#Sacamos Index.php y .htaccess a la carpeta raíz
-#cp /var/www/html/wordpress/index.php /var/www/html
-#cp $HOME/public_practica08/.htaccess /var/www/html
+#Borramos index.html
+rm index.html
 
-#Modificamos el Index de la carpeta raíz
-#sed -i 's#wp-blog-header.php#/wordpress/wp-blog-header.php#' /var/www/html/index.php
-
-#Otorgamos permisos a www-data del contenido del dir. Web
-#chown www-data:www-data /var/www/html -R
 
 # Instalamos el paquete Servidor de NFS
 apt-get install nfs-kernel-server -y
